404171be3e3283434930b594c9b6f3649926a369
Rodrigo Pozo <rodrigo.pozo25@gmail.com>
2023-07-21 23:20:12 -0500

    add fake api server (json-server)

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
+import {Observable,of} from 'rxjs';
 import {Task} from '../Task';
 import {TASKS} from '../mock-task';
+import {HttpClient,HttpHeaders} from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl='http://localhost:3000/tasks'
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
-  getTasks():Task[]{
-    return TASKS;
+  getTasks():Observable<Task[]>{ //using observable
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
